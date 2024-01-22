f0a7723228bf7c1ceed8259fe58c3c5296952fc1
AJBrinas <drewbrinas@gmail.com>
2024-01-22 09:52:08 +0800

    revision

--- app/routes/disaster_plan.py
+++ app/routes/disaster_plan.py
@@ -6,14 +6,20 @@ from app.config.database import db_dependency
 from app.models import users
 from app.schemas import user_schema
 # from ..auth.oauth2 import get_current_user
-router = APIRouter(prefix="/disaster-plan",
+router = APIRouter(prefix="/disaster",
                    tags=["Disaster Plan"])
 
 
 temp = Jinja2Templates(directory="app/templates")
 
 
-@router.get("/")
+@router.get("/plan")
 def root(request: Request):
     return temp.TemplateResponse("disaster_plan.html",
+                                 {"request": request})
+
+
+@router.get("/contacts")
+def contact(request: Request):
+    return temp.TemplateResponse("disaster_plan_contact.html",
                                  {"request": request})
