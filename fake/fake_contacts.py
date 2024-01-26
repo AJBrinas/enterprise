6d1d125a714bce9774ae19050d4b38e2ef774227
AJBrinas <drewbrinas@gmail.com>
2024-01-26 17:46:21 +0800

    df

--- fake/fake_contacts.py
+++ fake/fake_contacts.py
@@ -17,19 +17,14 @@ relationships = ['mother', 'father', 'sister', 'brother', 'wife',
                  'husband', 'niece', 'uncle', 'aunt']
 
 # Generate 60 rows of fake data for the EmergencyContact model
-for i in range(1, 61):
+for i in range(1, 100):
     contact_name = fake.name()
     relationship = random.choice(relationships)
     contact_number = fake.phone_number()
     person_info = random.randint(1, 200)
     created_at = generate_random_date().strftime('%Y-%m-%d %H:%M:%S')
 
-    insert_query = f"""
-                INSERT INTO emergency_contacts (
-                    contact_name, relationship, contact_number,
-                    person_info, created_at)
-                   VALUES (
-                    '{contact_name}', '{relationship}',
-                    '{contact_number}', {person_info}, '{created_at}');"""
+    insert_query = f"""INSERT INTO emergency_contacts (contact_name, relationship, contact_number, person_info, created_at)
+                   VALUES ('{contact_name}', '{relationship}', '{contact_number}', {person_info}, '{created_at}');"""
 
     print(insert_query)
