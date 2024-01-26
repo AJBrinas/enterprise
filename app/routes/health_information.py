0a7408c07a2280fee22c3eb4c5cb0e9912730e7a
AJBrinas <drewbrinas@gmail.com>
2024-01-26 09:08:39 +0800

    changes

--- app/routes/health_information.py
+++ app/routes/health_information.py
@@ -4,10 +4,11 @@ from fastapi.responses import RedirectResponse, ORJSONResponse
 from fastapi.templating import Jinja2Templates
 from app.config.database import db_dependency
 from app.models.s_health import EmergencyContact as ec, MedicalHistory as mh, VaccinationRecord as vr, Medication as m, HealthInformation as hi
-from app.schemas import user_schema, s_health as schema
+from app.schemas import s_health as schema
+from app.models import s_health as models
 from typing import List
 from fastapi.encoders import jsonable_encoder
-import datetime
+from datetime import date
 from sqlalchemy import func
 from ..auth.oauth2 import get_current_user
 
@@ -24,7 +25,7 @@ temp = Jinja2Templates(directory="app/templates")
 async def read_item(id: int, db: db_dependency,
                     active: int = Depends(get_current_user)):
     if not active:
-        return RedirectResponse('http://127.0.0.1:8000/login',
+        return RedirectResponse(url='http://127.0.0.1:8000/login',
                                 status_code=status.HTTP_401_UNAUTHORIZED)
 
     health = db.query(hi).filter(hi.id == id).first()
@@ -48,7 +49,7 @@ def read_items(db: db_dependency, limit: int = 10):
 # Getting all vaccines related to the person
 @router.get("/info/vac/{id}")
 def read_person_vaccines(id: int, db: db_dependency):
-    person = db.query(hi).filter(hi.id == id).first()
+    person = db.query(hi.full_name).filter(hi.id == id).first()
     vaccines = db.query(vr).filter(vr.person_info == id).all()
     # vac = vaccines.
     return {'person': person, 'vaccines': vaccines}
@@ -90,22 +91,6 @@ def read_health(request: Request, db: db_dependency):
                                                          "health": health})
 
 
-# Eventd
-@router.get('/events')
-def get_vaccine(request: Request):
-
-    return temp.TemplateResponse('events.html',
-                                 {'request': request})
-
-
-# Disaster Plan
-@router.get('/disaster_response')
-def get_disaster(request: Request):
-
-    return temp.TemplateResponse('disaster_response.html',
-                                 {'request': request})
-
-
 # Get Health data without Contact Information with html
 @router.get("/infos/all/{id}")
 def read_all_health(db: db_dependency, id: int):
@@ -133,3 +118,44 @@ async def gender_all(db: db_dependency):
                        ).group_by(hi.gender).all()
     result = [{"gender": gender, "total": total} for gender, total in genders]
     return result
+
+
+@router.get('/add')
+def add_form(request: Request):
+    return temp.TemplateResponse("try.html", {'request': request})
+
+
+# From the Html FORM
+@router.post("/create", status_code=201)
+def create_information(db: db_dependency, id: int,
+                       illness: str = Form(...),
+                       medicine: str = Form(...),
+                       dosage: str = Form(...),
+                       frequency: str = Form(...),
+                       diagnosed: date = Form(...),
+                       person_info: str = Form(...)):
+    # TODO: Implement the function to save information into database
+    new = m(
+        illness=illness,
+        medicine=medicine,
+        dosage=dosage,
+        frequency=frequency,
+        diagnosed=diagnosed,
+        person_info=person_info
+    )
+    db.add(new)
+    db.commit()
+    db.refresh(new)
+    return new
+
+
+@router.post("/update")
+def put_information(name: str = Form(), id: str = Form()):
+    # TODO: Implement the function to save information into database
+
+    return {'name': name, 'id': id}
+
+@router.put("/delete")
+def delete_information(db: db_dependency, id: int):
+    # TODO: Implement the function to save information into database
+    pass
